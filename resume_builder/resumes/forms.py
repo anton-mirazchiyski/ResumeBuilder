b8e70fe389da5affd5cd70512c5a5fcfb15823fe
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-06-25 23:18:40 +0300

    Start page for creating a resume

--- resume_builder/resumes/forms.py
+++ resume_builder/resumes/forms.py
@@ -6,4 +6,18 @@ from resume_builder.resumes.models import GeneralResume
 class GeneralResumeForm(forms.ModelForm):
     class Meta:
         model = GeneralResume
-        fields = '__all__'
+        exclude = ['resume_name']
+
+        # labels = {
+        #     'person_name': 'Name',
+        # }
+
+        widgets = {
+
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(GeneralResumeForm, self).__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            field.widget.attrs['class'] = 'general-resume-form-input'
+            field.widget.attrs['size'] = '35'
