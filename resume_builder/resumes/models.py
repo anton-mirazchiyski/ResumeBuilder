566887ff974cc9e0f71641a586ff844ae4c87fb2
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-06-25 13:15:16 +0300

    Add more fields to the base resume class

--- resume_builder/resumes/models.py
+++ resume_builder/resumes/models.py
@@ -8,7 +8,7 @@ class BaseResume(models.Model):
 
     resume_name = models.CharField(max_length=MAX_LENGTH_RESUME_NAME, unique=True, null=False, blank=True)
 
-    person_name = models.CharField(max_length=MAX_LENGTH_PERSON_NAME, null=False, blank=True)
+    person_name = models.CharField(max_length=MAX_LENGTH_PERSON_NAME, null=False, blank=False)
 
     email = models.EmailField(null=False, blank=False)
 
@@ -16,6 +16,17 @@ class BaseResume(models.Model):
 
     linkedin_profile = models.URLField(null=True, blank=True)
 
+    summary = models.TextField(null=True, blank=True)
+
+    education = models.TextField(null=False, blank=False)
+
+    work_experience = models.TextField(null=True, blank=True)
+
+    skills = models.TextField(null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=True)
 
     updated_at = models.DateTimeField(auto_now=True, null=False, blank=True)
+
+    class Meta:
+        abstract = True
